8c7ed3387fcbe4055e66c8ad5fd2aa307eba3d12
rasvi146 <rasvi146@student.liu.se>
2016-11-03 11:14:18 +0100

    Code cleaning

--- SensorFusion/Tracking/trajectory.m
+++ SensorFusion/Tracking/trajectory.m
@@ -14,18 +14,17 @@ classdef trajectory
             
         end
         function obj=add_data(obj,inp_data)
-            disp('1')
+            
             obj.y=[obj.y inp_data];
-            disp('2')
-            length(obj.y)-10>length(obj.traj)
-            disp('wtf')
+            
             if length(obj.y)-25>length(obj.traj)
-                disp('4')
+                
                obj.traj=rt_smooth(obj.y,obj.traj);
-               disp('3')
+            
             end
+            
         end
-    end
    end
+end
 
 
