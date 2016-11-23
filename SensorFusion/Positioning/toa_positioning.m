ab8e2370fd5e474b61afb9c8dcf509a44c9fb936
Markus <marpe163@student.liu.se>
2016-11-23 13:04:49 +0100

    TOA Positioning in 3D for an arbitrary number of sensors implemented.

--- SensorFusion/Positioning/toa_positioning.m
+++ SensorFusion/Positioning/toa_positioning.m
@@ -5,28 +5,32 @@ function xopt=toa_positioning(sensor_positions, ranges,grid)
 %minima. Note that the execution time is proportional to the square of the
 %length of the grid vector.
 %
-%Example:
-% traj=[5*cos(0:0.05:pi);5*sin(0:0.05:pi)];
-% senspos=[1 0;2 2;0 0];
-% dist=zeros(3,length(traj));
+% %Example:
+% traj=[cos(0:0.05:2*pi);sin(0:0.05:2*pi)]
+% traj=[traj;linspace(0,1,length(0:0.05:2*pi))];
+% senspos=[0 0 0; 0 1 0;0 0 1; 1 0 0]
+% dist=zeros(4,length(traj));
 % for it=1:length(traj)
-%    dist(1,it)=sqrt((traj(1,it)-senspos(1,1))^2 +(traj(2,it)-senspos(1,2))^2);
-%    dist(2,it)=sqrt((traj(1,it)-senspos(2,1))^2 +(traj(2,it)-senspos(2,2))^2);
-%    dist(3,it)=sqrt((traj(1,it)-senspos(3,1))^2 +(traj(2,it)-senspos(3,2))^2); 
+%     
+%    dist(1,it)=sqrt((traj(1,it)-senspos(1,1))^2 +(traj(2,it)-senspos(1,2))^2+(traj(3,it)-senspos(1,3))^2);
+%    dist(2,it)=sqrt((traj(1,it)-senspos(2,1))^2 +(traj(2,it)-senspos(2,2))^2+(traj(3,it)-senspos(2,3))^2);
+%    dist(3,it)=sqrt((traj(1,it)-senspos(3,1))^2 +(traj(2,it)-senspos(3,2))^2)+(traj(3,it)-senspos(3,3))^2;
+%    dist(4,it)=sqrt((traj(1,it)-senspos(4,1))^2 +(traj(2,it)-senspos(4,2))^2)+(traj(3,it)-senspos(4,3))^2; 
 % end
 % 
-% dist=dist+0.15*randn(size(dist));
+% dist=dist+0.05*randn(size(dist));
 % xpos=[];
-% for it=1:length(dist)
+% 
+% for it=1:length(traj) 
 %     it
 %     tic
-%     xpos=[xpos toa_positioning(senspos,[dist(1,it);dist(2,it);dist(3,it)],[-20 0 20])];
+%     xpos=[xpos toa_positioning(senspos,dist(:,it),[-5  5])];
 %     toc
 % end
 % clf
-% plot(traj(1,:),traj(2,:))
+% plot3(traj(1,:),traj(2,:),traj(3,:));
 % hold on
-% plot(xpos(1,:),xpos(2,:))
+% plot3(xpos(1,:),xpos(2,:),xpos(3,:))
 dmin=100000000000; %big number
 xopt=[0;0];
 opts = optimoptions('lsqnonlin','Display','off'); %Dont display warning from the nls solver.
@@ -34,12 +38,14 @@ opts = optimoptions('lsqnonlin','Display','off'); %Dont display warning from the
 %iterate over the grid
 for it=1:length(grid)
     for jt=1:length(grid)
-        %Utilize matlab's build in NLS solver.
-        [x,d] = lsqnonlin(@(x) costfcn(x,[sensor_positions(1,:);sensor_positions(2,:);sensor_positions(3,:)]...
-            ,[ranges(1);ranges(2);ranges(3)]),[grid(it);grid(jt)],[-20;-20],[20;20],opts);      
-        if d<dmin
-            dmin=d;
-            xopt=x;
+        for kt=1:length(grid)
+            %Utilize matlab's build in NLS solver.
+            [x,d] = lsqnonlin(@(x) costfcn(x,sensor_positions(:,:)...
+                ,ranges(:)),[grid(it);grid(jt);grid(kt)],[-20;-20;-20],[20;20;20],opts);      
+            if d<dmin
+                dmin=d;
+                xopt=x;
+            end
         end
     end
     
@@ -49,10 +55,11 @@ end
     function F = costfcn(x,pos,ranges)%remove ranges since its a nested fcn?
             distances=zeros(size(pos,1),1);
             for lt=1:length(distances)
-               distances(lt)=sqrt((x(1)-pos(lt,1))^2+(x(2)-pos(lt,2))^2) ;
+               distances(lt)=sqrt((x(1)-pos(lt,1))^2+(x(2)-pos(lt,2))^2+(x(3)-pos(lt,3))^2); 
             end
             F=zeros(length(distances),1);
             for lt=1:length(F)
+                
                F(lt)=ranges(lt)-distances(lt); 
             end
     end
