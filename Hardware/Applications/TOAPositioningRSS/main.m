126df04c87d6f21e2bbcb0cd50fa3d9664bd410c
Jack <chiya469@student.liu.se>
2016-12-01 17:01:25 +0000

    Add 2D TOA positioning

--- Hardware/Applications/TOAPositioningRSS/main.m
+++ Hardware/Applications/TOAPositioningRSS/main.m
@@ -15,7 +15,7 @@ senspos=[
      4.95    0  1.6;
      -2.1  2.3 1.95;
     25.25 2.25 1.05;
-     4.95    7    1;
+     9.75 0.85  2.4;
     -2.25    7    1];
 
 if ~exist('a','var') || ~isvalid(a)
@@ -28,42 +28,54 @@ xpos = [];
 while true
     % Get a data point
     data = a.readLatest;
-    fprintf('%d %d %d %d %d %d\n',...
+    fprintf('%d %d %d %d %d %d\n\n',...
         data(1), data(2), data(3), data(4), data(5), data(6));
     distance = data(1:6) / 1000;
     RSS = data(7:12);
-    sensname_index = 1:6;
+    sensor_index = 1:6;
 
     % Filter out the outlier values
-    tmp = [distance'; RSS'; sensname_index];
-    tmp = tmp(:,tmp(1,:)<10);
+    tmp = [distance'; RSS'; sensor_index];
+    tmp = tmp(:,tmp(1,:)<50);
     tmp = tmp(:,tmp(2,:)<0);
     tmp = tmp(:,tmp(2,:)>-200);
 
     % Break if we have less than four data points
-    if(size(tmp,2) < 4)
+    if(size(tmp,2) < 3)
         continue;
     end
 
     distance = tmp(1,:);
     RSS = tmp(2,:);
-    sensname_index = tmp(3,:);
+    sensor_index = tmp(3,:);
     [RSS_sorted, index] = sort(RSS,'descend');
     distance_sorted = distance(index);
-    sensname_sorted = sensname_index(index);
-    fprintf('%s %s %s %s\n',...
-        sensname(sensname_sorted(1),:),...
-        sensname(sensname_sorted(2),:),...
-        sensname(sensname_sorted(3),:),...
-        sensname(sensname_sorted(4),:));
+    sensor_index_sorted = sensor_index(index);
 
     % Take the four measurements with the best RSS
-    d = distance_sorted(1:4);
-    xpos=[xpos toa_positioning(senspos(index(1:4),:),d',[-5  10])];
+    if length(distance_sorted) > 3
+        d = distance_sorted(1:4);
+%         sensor_index_sorted(1:4)
+        xpos=[xpos toa_positioning(senspos(sensor_index_sorted(1:4),:),d',[-5  10])];
+    fprintf('%s %s %s %s\n',...
+        sensname(sensor_index_sorted(1),:),...
+        sensname(sensor_index_sorted(2),:),...
+        sensname(sensor_index_sorted(3),:),...
+        sensname(sensor_index_sorted(4),:));
+    elseif length(distance_sorted) == 3
+        fprintf('3oiuytre\n');
+        d = distance_sorted(1:3);
+%         sensor_index_sorted(1:3)
+        xpos=[xpos [toa_positioning2D(senspos(sensor_index_sorted(1:3),:),d',[-5  10]); 0]];
+    fprintf('%s %s %s\n',...
+        sensname(sensor_index_sorted(1),:),...
+        sensname(sensor_index_sorted(2),:),...
+        sensname(sensor_index_sorted(3),:));
+    end
     fprintf('%d %d\n', xpos(1,end), xpos(2,end));
 
-    %clf
-    %plot(xpos(1,:),xpos(2,:));
-    %hold on
-    %drawnow;
+    clf
+    plot(xpos(1,:),xpos(2,:));
+    hold on
+    drawnow;
 end
