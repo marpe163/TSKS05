008bed92df07b2bb3b7e1fa8f04d7730cf9f985b
Jack <chiya469@student.liu.se>
2016-12-02 14:09:46 +0000

    Compare with Pozyx and improve reporting in TOAPositioningRSS

--- Hardware/Applications/TOAPositioningRSS/main.m
+++ Hardware/Applications/TOAPositioningRSS/main.m
@@ -11,28 +11,29 @@ sensname=[
     '0x603B';
 ];
 senspos=[
-    14.75  0.3  1.6;
-     4.95    0  1.6;
-     -2.1  2.3 1.95;
+    14.75 0.30 1.60;
+     4.95 0.00 1.60;
+    -2.10 2.30 1.95;
     25.25 2.25 1.05;
-     9.75 0.85  2.4;
-    -2.25    7    1];
+     9.75 1.90 2.40;
+    -2.25 7.00 1.00];
 
 if ~exist('a','var') || ~isvalid(a)
     % Open the serial port connection
-    a = Arduino('/dev/ttyS99','%d %d %d %d %d %d %d %d %d %d %d %d');
+    a = Arduino('/dev/ttyS99','%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d');
 end
 
+pos = [];
 xpos = [];
 
 while true
     % Get a data point
     data = a.readLatest;
-    fprintf('%d %d %d %d %d %d\n\n',...
-        data(1), data(2), data(3), data(4), data(5), data(6));
     distance = data(1:6) / 1000;
     RSS = data(7:12);
     sensor_index = 1:6;
+    % Position reported by Pozyx
+    pos = [pos [data(13);data(14);data(15)]/1000];
 
     % Filter out the outlier values
     tmp = [distance'; RSS'; sensor_index];
@@ -45,6 +46,7 @@ while true
         continue;
     end
 
+    % Sort the data by RSS
     distance = tmp(1,:);
     RSS = tmp(2,:);
     sensor_index = tmp(3,:);
@@ -55,27 +57,31 @@ while true
     % Take the four measurements with the best RSS
     if length(distance_sorted) > 3
         d = distance_sorted(1:4);
-%         sensor_index_sorted(1:4)
         xpos=[xpos toa_positioning(senspos(sensor_index_sorted(1:4),:),d',[-5  10])];
-    fprintf('%s %s %s %s\n',...
-        sensname(sensor_index_sorted(1),:),...
-        sensname(sensor_index_sorted(2),:),...
-        sensname(sensor_index_sorted(3),:),...
-        sensname(sensor_index_sorted(4),:));
+        info_mode = sprintf('More than three anchors\n');
     elseif length(distance_sorted) == 3
-        fprintf('3oiuytre\n');
         d = distance_sorted(1:3);
-%         sensor_index_sorted(1:3)
         xpos=[xpos [toa_positioning2D(senspos(sensor_index_sorted(1:3),:),d',[-5  10]); 0]];
-    fprintf('%s %s %s\n',...
-        sensname(sensor_index_sorted(1),:),...
-        sensname(sensor_index_sorted(2),:),...
-        sensname(sensor_index_sorted(3),:));
+        info_mode = sprintf('Three anchors\n');
     end
-    fprintf('%d %d\n', xpos(1,end), xpos(2,end));
 
+    % Report some information in command window
+    info_data = sprintf('%d %d %d %d %d %d\n',...
+        data(1), data(2), data(3), data(4), data(5), data(6));
+    info_xpos = sprintf('  TOA: %6.3f %6.3f\n', xpos(1,end), xpos(2,end));
+    info_pos  = sprintf('Pozyx: %6.3f %6.3f\n', pos(1,end), pos(2,end));
+    fprintf('\n\n%s%s%s%s', info_mode, info_data, info_xpos, info_pos);
+    for i=1:length(sensor_index_sorted)
+        fprintf('%s ', sensname(sensor_index_sorted(i),:));
+    end
+
+    % Plot data
     clf
-    plot(xpos(1,:),xpos(2,:));
+    plot(xpos(1,:),xpos(2,:),'x');
     hold on
+    plot(pos(1,:),pos(2,:),'o');
+    legend('TOA', 'Pozyx');
+    xlim([-5 30]);
+    ylim([-5 10]);
     drawnow;
 end
--- Hardware/Applications/TOAPositioningRSS/sketch_dec01b/sketch_dec01b.ino
+++ Hardware/Applications/TOAPositioningRSS/sketch_dec01b/sketch_dec01b.ino
@@ -6,7 +6,10 @@
 
 #define NUM_ANCHORS 6
 uint8_t num_anchors = NUM_ANCHORS;
-uint16_t anchors[NUM_ANCHORS] = {0x6078, 0x603F, 0x6050, 0x6000, 0x600E, 0x603B};
+uint16_t anchors[NUM_ANCHORS]  = {0x6078, 0x603F, 0x6050, 0x6000, 0x600E, 0x603B};
+int32_t anchors_x[NUM_ANCHORS] = { 14750,   4950,  -2100,  25250,   9750,  -2250};
+int32_t anchors_y[NUM_ANCHORS] = {   300,      0,   2300,   2250,   1900,   7000};
+int32_t anchors_z[NUM_ANCHORS] = {  1600,   1600,   1950,   1050,   2400,   1000};
 
 char buf[128];
 
@@ -20,9 +23,26 @@ void setup() {
     delay(100);
     abort();
   }
+
+  // add anchors
+  device_coordinates_t dev;
+  unsigned i;
+  for (i=0; i<NUM_ANCHORS; i++) {
+    dev.network_id = anchors[i];
+    dev.flag = 1;
+    dev.pos.x = anchors_x[i];
+    dev.pos.y = anchors_y[i];
+    dev.pos.z = anchors_z[i];
+    int result = Pozyx.addDevice(dev);
+    if (result != POZYX_SUCCESS) {
+      Serial.println(F("addDevice failed"));
+    }
+  }
 }
 
 void loop() {
+  coordinates_t position;
+  Pozyx.doPositioning(&position, POZYX_2_5D, 0);
   // uint32_t timestamp[NUM_ANCHORS];
   uint32_t distance[NUM_ANCHORS];
   int16_t RSS[NUM_ANCHORS];
@@ -41,6 +61,8 @@ void loop() {
     distance[0], distance[1], distance[2], distance[3], distance[4], distance[5],
     RSS[0], RSS[1], RSS[2], RSS[3], RSS[4], RSS[5]
   );
+  Serial.print(buf);
+  sprintf(buf, " %" PRIi32 " %" PRIi32 " %" PRIi32, position.x, position.y, position.z);
   Serial.println(buf);
 }
 
